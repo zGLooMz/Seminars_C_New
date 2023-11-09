c1831ef246472d97b25e73130c1385cdd252f685
Alexandr Zybailo <zero-pushkin@mail.ru>
2023-11-09 18:55:31 +0300

    Add Seminar5

---
+++ Seminar5/CalculatorClass.cs
@@ -0,0 +1,66 @@
+﻿﻿﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Seminar5
+{
+    class CalculatorClass : ICalculator
+    {
+        private double result;
+        public double Result { get { return result; } }
+        public event EventHandler<EventArgs>? MyEventHandler;
+        private Stack<double> stack;
+
+        internal CalculatorClass() 
+        {
+            result = 0;
+            stack = new Stack<double>(10);
+        }
+        private void PrintResult()
+        {
+            MyEventHandler?.Invoke(this, new EventArgs());
+        }
+
+        public void Divide(double x)
+        {
+            if (x == 0)
+                return;
+            stack.Push(result);
+            result /= x;
+            PrintResult();
+        }
+
+        public void Multy(double x)
+        {
+            stack.Push(result);
+            result *= x;
+            PrintResult();
+        }
+
+        public void Sub(double x)
+        {
+            stack.Push(result);
+            result -= x;
+            PrintResult();
+        }
+
+        public void Sum(double x)
+        {
+            stack.Push(result);
+            result += x;
+            PrintResult();
+        }
+
+        public void Cansale()
+        {
+            if (stack.Count == 0)
+                return;
+            result = stack.Pop();
+            PrintResult();
+        }
+    }
+}
---
+++ Seminar5/ICalculator.cs
@@ -0,0 +1,19 @@
+﻿﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar5
+{
+    interface ICalculator
+    {
+        double Result { get;}
+        void Sum(double x);
+        void Sub(double x);
+        void Multy(double x);
+        void Divide(double x);
+        event EventHandler<EventArgs> MyEventHandler;
+        void Cansale();
+    }
+}
